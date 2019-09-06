edc3528066ae5a7e29ddcb26142c8e08e27534e1
Jonathan Moore <jonathan.moore6247@gmail.com>
2019-09-06 09:48:39 -0500

    Favicon

--- src/routes/ProjectsPage/ProjectsPage.js
+++ src/routes/ProjectsPage/ProjectsPage.js
@@ -48,7 +48,7 @@ export default class ProjectsPage extends React.Component{
           </p>
         </li>
         <li>
-          <p>USES: HTML, CSS, Javascript, react.js, node.js, NPM Packages, and Webpack</p>
+          <p>USES: HTML, CSS, Javascript, react.js, node.js, knex, sql, NPM Packages, and Webpack</p>
         </li>
       </ul>
         <img className="image" src={require("../../Images/spaced-rep.png")} alt="spaced-repetition" />
@@ -62,6 +62,28 @@ export default class ProjectsPage extends React.Component{
           <button className="github-button"> Github Backend</button>
         </a>
         <a href="https://aldiyar-jonathan-spaced-repetition.now.sh/" target="blank">
+          <button className="live-link"> Live Demo</button>
+        </a>
+        <ul className="project-details">
+        <li>
+          <p>PROJECT TITLE: Jobreel
+          </p>
+        </li>
+        <li>
+          <p>USES: HTML5, CSS3, ReactJS, NodeJS, Express, Enzyme, Mocha, Chai, PostgreSQL</p>
+        </li>
+      </ul>
+        <img className="image" src={require("../../Images/jobreel.jpg")} alt="spaced-repetition" />
+        <p>JobReel simplifies the job search process for Thinkful Graduates. THe app is loaded with 3rd party API's to assist you in job search. Our database can hold entries such as jobs that user applied to, connects with people, and companies of interest. User is greated with user friendly dsahboard that has all information needed to start searching for the job and connect with people at different companies. The app is responsive and can be used on desktop, laptop, tablet, or phone! Choice is yours! JobReel is you job search companion! My responsibilities on the project included construction of the server using nodeJs and express and construction of the database using postgres. I also assisted in building a number of the frontend components. I was also responsible for quality control of the app for both frontend and backend</p>
+        <a href="https://github.com/thinkful-ei-dragonfly/JobReel-client"
+          target="blank">
+          <button className="github-button">Github Frontend</button>
+        </a>
+        <a href="https://github.com/thinkful-ei-dragonfly/JobReel-server"
+        target="blank">
+          <button className="github-button"> Github Backend</button>
+        </a>
+        <a href="https://jobreel.now.sh/" target="blank">
           <button className="live-link"> Live Demo</button>
         </a>
       </section>
